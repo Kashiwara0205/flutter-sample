371d20487560ec621a7e8dc0a6addf4dd511b456
kashiwara <tanakatarou0205@gmail.com>
2021-07-23 12:21:28 +0900

    Add Icon

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class RandomWords extends StatefulWidget{
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = <WordPair>{};
 
   @override
   Widget build(BuildContext context) {
@@ -42,11 +43,25 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap:(){
+        setState(() {
+          if (alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
 }
